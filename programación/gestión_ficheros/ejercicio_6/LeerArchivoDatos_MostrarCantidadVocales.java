fd03113ab6cde4b2f568c29f289bf6ffbd861e0b
sufigueroa87 <sufigueroa87@gmail.com>
2022-12-18 19:10:43 +0100

    finalizo uso fichero

--- programación/gestión_ficheros/ejercicio_6/LeerArchivoDatos_MostrarCantidadVocales.java
+++ programación/gestión_ficheros/ejercicio_6/LeerArchivoDatos_MostrarCantidadVocales.java
@@ -14,19 +14,18 @@ public class LeerArchivoDatos_MostrarCantidadVocales {
 
     public void inici(){
 
-        File f = new File("src/unitat6/u6_2_TractamentBasicDades/cursoProgramacionJava/AccesoAFicheros/LeerArchivosDatos_MostrarCantidadVocales/datos.txt");
+        cantidadVocalesFile("src/unitat6/u6_2_TractamentBasicDades/cursoProgramacionJava/AccesoAFicheros/LeerArchivosDatos_MostrarCantidadVocales/datos.txt");
+    }
+
+    public void cantidadVocalesFile(String ruta){
+        File f = new File(ruta);
         FileReader fr = null;
         try{
-            fr = new FileReader("src/unitat6/u6_2_TractamentBasicDades/cursoProgramacionJava/AccesoAFicheros/LeerArchivosDatos_MostrarCantidadVocales/datos.txt");
+            fr = new FileReader(ruta);
         }
         catch(IOException e){
             System.err.println("Problemas con el archivo.");
         }
-
-        cantidadVocalesFile(f, fr);
-    }
-
-    public void cantidadVocalesFile(File f, FileReader fr){
         int cantidadVocales = 0;
         for (int i = 0; i < f.length(); i++){
             try{
@@ -40,7 +39,13 @@ public class LeerArchivoDatos_MostrarCantidadVocales {
                 System.out.println("Problemas con el archivo.");
             }
         }
+        try{
+            fr.close();
+
+        }
+        catch(IOException e){
+            System.out.println("Problemas con el archivo " + f.getName());
+        }
         System.out.println("La cantidad de vocales que contiene el archivo " + f.getName() + " es " + cantidadVocales);
     }
-
 }
